429f9ed96c8e094e3576afd228143dfdd338ee4b
周泽强 <18983678350@189.cn>
2026-07-17 09:04:50 +0800

    Revert "feat: add CODE_EXTENSIONS and isCodeExtension()"
    This reverts commit 8929e0588419f76cf378cf3a142c197d649d1d36.

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -35,28 +35,6 @@ export function isFileTypeVisible(
   return !hiddenExtensions.includes(extension);
 }
 
-const CODE_EXTENSIONS = new Set([
-  ".txt", ".sql", ".java", ".py", ".js", ".mjs", ".cjs", ".ts", ".tsx", ".jsx",
-  ".json", ".xml", ".html", ".htm", ".vue", ".yaml", ".yml", ".css", ".scss",
-  ".sass", ".less", ".styl", ".sh", ".bash", ".zsh", ".ps1", ".psm1", ".psd1",
-  ".bat", ".cmd", ".php", ".php3", ".php4", ".php5", ".phtml", ".rb", ".go", ".rs",
-  ".c", ".h", ".cpp", ".cc", ".cxx", ".hpp", ".hxx", ".cs", ".swift", ".kt", ".kts",
-  ".scala", ".groovy", ".gradle", ".pl", ".pm", ".lua", ".r", ".R", ".rmd", ".m", ".mm",
-  ".dart", ".erl", ".ex", ".exs", ".coffee", ".tex", ".sty", ".cls", ".ltx", ".rst",
-  ".toml", ".ini", ".cfg", ".conf", ".env", ".properties", ".makefile", ".Makefile",
-  ".dockerfile", ".Dockerfile", ".gitignore",
-]);
-
-const CODE_FILENAMES = new Set([
-  "makefile", "Makefile", "dockerfile", "Dockerfile", "gitignore",
-]);
-
-export function isCodeExtension(filename: string): boolean {
-  if (CODE_FILENAMES.has(filename)) return true;
-  const ext = getExtension(filename);
-  return CODE_EXTENSIONS.has(ext);
-}
-
 export type FileTypeCategory = "markdown" | "office" | "image" | "pdf" | "other";
 
 export function getFileType(filename: string): FileTypeCategory {
